6eb91217fa558969b833fca812f9c923bf8988e0
master <papachristos.andreas@gmail.com>
2023-01-23 21:59:16 +0200

    initial

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+from math import sqrt
+
+
+
+
+def PerfectNumbers(number):
+    sum = 0
+    for i in range(2, int(math.ceil(sqrt(number)))):
+
+        if int(number) % i == 0:
+            sum += i+(number/i)
+
+    if (sum +1 == number):
+        print(number)
+    else:
+        print("not a perfect number")
+        print(sum)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+   PerfectNumbers(28)
+
+
